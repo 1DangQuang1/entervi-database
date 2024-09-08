588edf13973d1fd9c7240483e7b373ac3d29b9da
1DangQuang1 <116643485+1DangQuang1@users.noreply.github.com>
2024-09-08 17:26:17 +0700

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for the database to be ready
 echo "Waiting for the database to be ready..."
-sleep 10  # Tăng thời gian chờ nếu cần, tùy thuộc vào độ chậm của việc khởi động database.
+sleep 10  
 
 # Initialize the Airflow database
 echo "Initializing Airflow database..."
